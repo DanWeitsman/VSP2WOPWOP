Fixed periodic loading/removed bug associated with CW rotation flag
PSU-WOPWOP would throw an error when running periodic loading cases since the rotational rate (omega) input argument was not provided for the PeriodicLoadingPatchFileWrite.py module. This quantity is now extracted from the provided .mat file and passed along to the function. Additionally, the loading vectors in hover would be be incorrectly flipped radially if the CW rotation flag is enabled in the input.py module. This has also been fixed.

VSP2WOPWOP.py
@@ -97,8 +97,8 @@ def main():
                         loadParams = {'dFx':mat_read[args.rotor]['inplane'][args.azimuth,:],'dFy':np.zeros(np.shape(mat_read[args.rotor]['inplane'][args.azimuth,:])),'dFz':mat_read[args.rotor]['outplane'][args.azimuth,:],'th':[0,0,0]}
                         ConstantLoadingPatchFileWrite(UserIn['loadingFileName'], loadParams, geomParams['nXsecs'], dirSaveFile)
                     else:
-                        loadParams = {'dFx':mat_read[args.rotor]['inplane'][:],'dFy':np.zeros(np.shape(mat_read[args.rotor]['inplane'][:])),'dFz':mat_read[args.rotor]['outplane'][:],'th':[0,0,0],'phi': np.linspace(0,2*np.pi,np.shape(mat_read[args.rotor]['inplane'][:])[0])}
-                        PeriodicLoadingPatchFileWrite(UserIn['loadingFileName'], loadParams, geomParams['nXsecs'], omega,dirSaveFile)
+                        loadParams = {'dFx':mat_read[args.rotor]['inplane'][:],'dFy':np.zeros(np.shape(mat_read[args.rotor]['inplane'][:])),'dFz':mat_read[args.rotor]['outplane'][:],'th':[0,0,0],'phi': np.linspace(0,2*np.pi,np.shape(mat_read[args.rotor]['inplane'][:])[0]+1)}
+                        PeriodicLoadingPatchFileWrite(UserIn['loadingFileName'], loadParams, geomParams['nXsecs'], np.squeeze(mat_read[args.rotor]['rpm'][()]) ,dirSaveFile)
 
                 else:
                     loadParams = loadingHover(UserIn, geomParams, XsecPolar_select, T, omega, Vz)

loadingHover.py
@@ -252,12 +252,12 @@ def loadingHover(UserIn, geomParams, XsecPolar, T, omega, Vz):
     dFy = np.zeros(len(r))
 
     #   if the rotor is rotating CW the force distributions are flipped along the longitudinal axis of the rotor disk.
-    if UserIn['rotation'] == 2:
-        dFz = np.flip(dFz,axis = 0)
-        dFx = np.flip(dFx, axis=0)
-        AoA = np.flip(AoA, axis=0)
-        lam = np.flip(lam, axis=0)
-        U = np.flip(U, axis=0)
+    # if UserIn['rotation'] == 2:
+    #     dFz = np.flip(dFz,axis = 0)
+    #     dFx = np.flip(dFx, axis=0)
+    #     AoA = np.flip(AoA, axis=0)
+    #     lam = np.flip(lam, axis=0)
+    #     U = np.flip(U, axis=0)
 
 
 #%%